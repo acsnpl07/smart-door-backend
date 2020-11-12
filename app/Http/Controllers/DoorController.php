abbe9888bb7264e8a6d0581af1dc7189b8f0c20a
acsnpl07 <Acsnpl07@gmail.com>
2020-11-12 13:20:40 +0200

    fine refactoring

--- app/Http/Controllers/DoorController.php
+++ app/Http/Controllers/DoorController.php
@@ -16,19 +16,18 @@ class DoorController extends Controller
         ];
     }
 
-
     public function open()
     {
-        $authed_user =Auth::user();
+        $authed_user = Auth::user();
         $door = Door::first();
         if ($door->is_closed == 1) {
             // TODO: issue the resperri by to open is, then save the state to the database
             $door->is_closed = 0;
             $door->save();
             DoorLog::create([
-                'name'=> $authed_user->name,
+                'name' => $authed_user->name,
                 'entered' => true,
-                'is_camera' =>false
+                'is_camera' => false
             ]);
             return ["Door is now open"];
         }
@@ -38,14 +37,12 @@ class DoorController extends Controller
 
     public function close()
     {
-        $authed_user =Auth::user();
+
         $door = Door::first();
         if ($door->is_closed != 1) {
             // TODO: issue the resperri by to open is, then save the state to the database
             $door->is_closed = 1;
             $door->save();
-
-
             return ["Door is now closed"];
         }
 
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
 Route::group(['middleware' => 'auth:sanctum'], function () {
     //All secure URL's
     Route::group(['prefix' => 'user'], function () {
