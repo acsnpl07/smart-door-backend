61d53f6e4be326a5161e3acb8a16beee5070e79d
acsnpl07 <Acsnpl07@gmail.com>
2020-11-18 18:22:52 +0200

    order log by createion time

--- app/Http/Controllers/DoorLogController.php
+++ app/Http/Controllers/DoorLogController.php
@@ -10,7 +10,7 @@ class DoorLogController extends Controller
 
     public function index()
     {
-        return DoorLog::paginate(15);
+        return DoorLog::orderBy('created_at' , 'desc')->paginate(15);
     }
 
     public function storeFromPi()
