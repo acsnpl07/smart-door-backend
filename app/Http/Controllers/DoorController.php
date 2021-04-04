8596accd19f86e4c502ae4a64b78e48e42b1f29e
acsnpl07 <Acsnpl07@gmail.com>
2021-04-05 01:45:03 +0200

    fixed ping

--- app/Http/Controllers/DoorController.php
+++ app/Http/Controllers/DoorController.php
@@ -56,23 +56,12 @@ class DoorController extends Controller
 
     public function ping()
     {
-        UpdateDoorAttributes::dispatchAfterResponse(request()->door,$this->getIp());
+        $clientIP = \Request::ip();
+        UpdateDoorAttributes::dispatchAfterResponse(request()->door, $clientIP);
         return response()->json([
             'message' => 'pong',
             'is_closed' => request()->door->is_closed,
         ]);
     }
-    public function getIp(){
-        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
-            if (array_key_exists($key, $_SERVER) === true){
-                foreach (explode(',', $_SERVER[$key]) as $ip){
-                    $ip = trim($ip); // just to be safe
-                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
-                        return $ip;
-                    }
-                }
-            }
-        }
-        return request()->ip(); // it will return server ip when no client ip found
-    }
+
 }
