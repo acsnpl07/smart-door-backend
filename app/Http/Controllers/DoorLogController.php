4a927b28637abfac2e054ea18e7028200ffdd19b
acsnpl07 <Acsnpl07@gmail.com>
2021-04-05 01:05:44 +0200

    fixed get client ip address

--- app/Http/Controllers/DoorLogController.php
+++ app/Http/Controllers/DoorLogController.php
@@ -34,6 +34,8 @@ class DoorLogController extends Controller
             [
                 'image_url' => 'required',
                 'entered' => ['required', 'boolean'],
+
+
                 'door_id' => ['in:1,2']
             ]
         );
